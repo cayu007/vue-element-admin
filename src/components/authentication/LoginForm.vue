a0cc4e24ad58e0e3efcecc2602a6414ff28fb3be
cayu007 <60328165+cayu007@users.noreply.github.com>
2023-12-19 23:58:58 +0100

    Update LoginForm.vue

--- src/components/authentication/LoginForm.vue
+++ src/components/authentication/LoginForm.vue
@@ -1,1 +1,41 @@
-
+<template>
+    <div>
+      <h2>Autenticación WordPress</h2>
+      <input v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button @click="handleLogin">Login</button>
+      <p v-if="errorMessage" style="color: red">{{ errorMessage }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import { authenticateWithWordPress } from '@/api/wordpress';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+        errorMessage: ''  // Mensaje de error para mostrar al usuario
+      };
+    },
+    methods: {
+      async handleLogin() {
+        if (!this.username || !this.password) {
+          this.errorMessage = 'Por favor, introduzca un nombre de usuario y contraseña.';
+          return;
+        }
+  
+        try {
+          const token = await authenticateWithWordPress(this.username, this.password);
+          console.log("Token received:", token);
+          this.$emit('login', token);
+          this.errorMessage = '';
+        } catch (error) {
+          console.error("Error during authentication:", error);
+          this.errorMessage = 'Error de autenticación. Por favor, intente de nuevo.';
+        }
+      }
+    }
+  };
+  </script>
