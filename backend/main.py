11afdd5499e95b58c5ceb93f4fb763e2194244fc
cayu007 <60328165+cayu007@users.noreply.github.com>
2023-12-20 06:32:43 +0100

    corss

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Configura CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://delightful-sand-0a1c96803.4.azurestaticapps.net"],  # Añade aquí el dominio de tu frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/hola")
-def leer_raiz():
-    return {"Hola": "Mundo"}
+def hola_mundo():
+    return {"mensaje": "Hola Mundo"}
