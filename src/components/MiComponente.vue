ddce7c544bfdbd7419a2105a93d3b55af2eb29c7
cayu007 <60328165+cayu007@users.noreply.github.com>
2023-12-20 06:01:19 +0100

    act backend

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button @click="cargarDatosDePython">Cargar Datos</button>
+    <p v-if="mensaje">{{ mensaje }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      mensaje: ''
+    }
+  },
+  methods: {
+    async cargarDatosDePython() {
+      try {
+        const response = await axios.get('https://apipythondash.azurewebsites.net/')
+        this.mensaje = response.data.mensaje
+      } catch (error) {
+        console.error('Error al cargar datos de Python:', error)
+      }
+    }
+  }
+}
+</script>
